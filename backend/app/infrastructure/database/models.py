303db30e408c5c7e56d7fb0c0fc763c7bbde243c
Alex <kouvira679@gmail.com>
2026-03-05 14:48:25 -0800

    fixing a typo

--- backend/app/infrastructure/database/models.py
+++ backend/app/infrastructure/database/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
-class Custaomer(Base):
+class Customer(Base):
     __tablename__ = "customers"
 
     # Passwords can be the same, but usernames and emails must be unique
