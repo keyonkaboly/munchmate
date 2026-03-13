57012b3d0be6a5ff6c539421a83bc17d5cf02981
Keyon Kaboly <106369493+keyonkaboly@users.noreply.github.com>
2026-03-13 12:55:13 -0700

    [F3-US2] Restaurant Specific Search Bar + Minor bugfix thatll fix pytests and Pagination for Search for restaurant menu items and restaurants (#127)
    * Made search feature for specific restaurant. Edited according tests
    
    * Fix for marcus to not return message
    
    * Pagination for menu items  search

--- backend/app/utils/filters.py
+++ backend/app/utils/filters.py
@@ -2,7 +2,7 @@ from app.infrastructure.database.models import Restaurant
 from sqlalchemy.orm import Session
 
 # takes the db connection and two optional filters (None means not provided)
-def apply_dietary_filters(db: Session, is_halal: bool = None, is_vegetarian: bool = None):
+def apply_dietary_filters(db: Session, is_halal: bool = None, is_vegetarian: bool = None, return_query: bool = False):
     # command to fetch all restaurants
     query = db.query(Restaurant)
     
@@ -12,4 +12,6 @@ def apply_dietary_filters(db: Session, is_halal: bool = None, is_vegetarian: boo
     if is_vegetarian is not None:
         query = query.filter(Restaurant.is_vegetarian == is_vegetarian)
 
+    if return_query:
+        return query
     return query.all()
