974a623a97c5c27e559975ede38acea06b878400
karrmh5 <k.hammouda@outlook.com>
2026-03-24 16:16:23 -0700

    Fix payment schema

--- backend/app/presentation/schemas/payment_schemas.py
+++ backend/app/presentation/schemas/payment_schemas.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class PaymentRequest(BaseModel):
     order_id: str
-    total_cost: float
     card_number: str
 
 class PaymentResponse(BaseModel):
