84e4528676ecc18e24944bef9b970124380e15fa
keyon <keyonkaboly@gmail.com>
2026-03-24 20:34:34 -0700

    Removing search already exists under get restaurant

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,36 +5,27 @@ from app.presentation.api.v1 import restaurants
 from app.presentation.api.v1 import checkout
 from app.presentation.api.v1 import payments
 from app.presentation.api.v1 import notifications
-from app.presentation.api.v1 import search
 from app.presentation.api.v1.order_router import router_order
 from app.data.dataset_seed import seed_dataset_data
 
 app = FastAPI(title="munchmate")
 
-# Creates tables
 Base.metadata.create_all(bind=engine)
 seed_dataset_data()
 
 
 app.include_router(router_auth)
 
-app.include_router(search.router)
-
-# Include restaurant router after authorization, making sure auth is checked before getting access to the restaurant endpoints
 app.include_router(restaurants.router)
 
-#create an order:
 app.include_router(router_order)
 
 app.include_router(checkout.router)
 
-# registers routes defined in payments.py with main FastAPI
 app.include_router(payments.router)
 
-# registers routes defined in notifications.py with main FastAPI
 app.include_router(notifications.router)
 
-# Creates endpoint
 @app.get("/")
 def root():
     return{"message": "Success: FastAPI is running."}
